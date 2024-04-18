a883caf3eeb3ffc3637a48bc9d0e1f59db6db357
Karim Metwaly <88909362+KarimMetwaly19@users.noreply.github.com>
2024-04-18 22:25:35 +0200

    trying working

--- train_tryon.py
+++ train_tryon.py
@@ -1625,41 +1625,41 @@ class Decoder_Block(nn.Module):
         d = torch.cat([d, g], axis=1)
         d = self.r1(d)
         return d
+#not working
+# class build_resunetplusplus(nn.Module):
+#     def __init__(self):
+#         super().__init__()
 
-class build_resunetplusplus(nn.Module):
-    def __init__(self):
-        super().__init__()
+#         self.c1 = Stem_Block(36, 64, stride=1)
+#         self.c2 = ResNet_Block(64, 128, stride=2)
+#         self.c3 = ResNet_Block(128, 256, stride=2)
+#         self.c4 = ResNet_Block(256, 512, stride=2)
 
-        self.c1 = Stem_Block(36, 64, stride=1)
-        self.c2 = ResNet_Block(64, 128, stride=2)
-        self.c3 = ResNet_Block(128, 256, stride=2)
-        self.c4 = ResNet_Block(256, 512, stride=2)
-
-        self.b1 = ASPP(256, 512)
-       # self.b1 = ASPP(128, 256)    
+#         self.b1 = ASPP(256, 512)
+#        # self.b1 = ASPP(128, 256)    
 	    
 
-        self.d1 = Decoder_Block([256, 512], 512)
-        self.d2 = Decoder_Block([128, 512], 256)
-        self.d3 = Decoder_Block([64, 256], 128)
-        self.output = nn.Conv2d(128, 4, kernel_size=1)
+#         self.d1 = Decoder_Block([256, 512], 512)
+#         self.d2 = Decoder_Block([128, 512], 256)
+#         self.d3 = Decoder_Block([64, 256], 128)
+#         self.output = nn.Conv2d(128, 4, kernel_size=1)
 
 
-    def forward(self, inputs):
-        c1 = self.c1(inputs)
-        c2 = self.c2(c1)
-        c3 = self.c3(c2)
-        c4 = self.c4(c3)
+#     def forward(self, inputs):
+#         c1 = self.c1(inputs)
+#         c2 = self.c2(c1)
+#         c3 = self.c3(c2)
+#         c4 = self.c4(c3)
 
-        b1 = self.b1(c4)
+#         b1 = self.b1(c4)
 
-        d1 = self.d1(c3, b1)
-        d2 = self.d2(c2, b1)
-        d3 = self.d3(c1, d2)
+#         d1 = self.d1(c3, b1)
+#         d2 = self.d2(c2, b1)
+#         d3 = self.d3(c1, d2)
 
-        output = self.output(d3)
+#         output = self.output(d3)
 
-        return output
+#         return output
 #habbooda version
     # def forward(self, inputs):
     #     c1 = self.c1(inputs)
@@ -1677,6 +1677,61 @@ class build_resunetplusplus(nn.Module):
 
     #     return output
 
+
+
+
+#working ta2reban
+#resunet++ small version
+class build_resunetplusplus(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.c1 = Stem_Block(36, 64, stride=1)
+        self.c2 = ResNet_Block(64, 128, stride=2)
+     #k   self.c3 = ResNet_Block(128, 256, stride=2)
+        #self.c4 = ResNet_Block(256, 512, stride=2)
+
+       #k self.b1 = ASPP(256, 512)
+        self.b1 = ASPP(128, 256)    
+	    
+
+        #self.d1 = Decoder_Block([256, 512], 512)
+     #k   self.d2 = Decoder_Block([128, 512], 256)
+        self.d3 = Decoder_Block([64, 256], 128)
+        self.output = nn.Conv2d(128, 4, kernel_size=1)
+
+
+    # def forward(self, inputs):
+    #     c1 = self.c1(inputs)
+    #     c2 = self.c2(c1)
+    #     c3 = self.c3(c2)
+    #     #c4 = self.c4(c3)
+
+    #     b1 = self.b1(c3)
+
+    #     #d1 = self.d1(c3, b1)
+    #     d2 = self.d2(c2, b1)
+    #     d3 = self.d3(c1, d2)
+
+    #     output = self.output(d3)
+
+    #     return output
+#habbooda version
+    def forward(self, inputs):
+        c1 = self.c1(inputs)
+        c2 = self.c2(c1)
+     #k c3 = self.c3(c2)
+        #c4 = self.c4(c3)
+
+        b1 = self.b1(c2)
+
+        #d1 = self.d1(c3, b1)
+     #k   d2 = self.d2(c2, b1)
+        d3 = self.d3(c1, b1)
+
+        output = self.output(d3)
+
+        return output
 # Converts a Tensor into a Numpy array
 # |imtype|: the desired type of the converted numpy array
 
