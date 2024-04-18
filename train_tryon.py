749ca8445f98412ccb3b765bc213dfdd4c26e7a2
Karim Metwaly <88909362+KarimMetwaly19@users.noreply.github.com>
2024-04-19 00:03:23 +0200

    fix3

--- train_tryon.py
+++ train_tryon.py
@@ -1634,12 +1634,12 @@ class build_resunetplusplus(nn.Module):
         self.c2 = ResNet_Block(64, 128, stride=2)
         self.c3 = ResNet_Block(128, 256, stride=2)
         self.c4 = ResNet_Block(256, 512, stride=2)
-	self.b1 = ASPP(512,1024)
+	
        # self.b1 = ASPP(256, 512)
        # self.b1 = ASPP(128, 256)  
 	 #haboda
 	    
-
+	self.b1 = ASPP(512, 1024)
         self.d1 = Decoder_Block([256, 1024], 512)
         self.d2 = Decoder_Block([128, 512], 256)
         self.d3 = Decoder_Block([64, 256], 128)
