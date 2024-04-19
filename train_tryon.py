53aa779cb634403f91a679280c26abbb672682e6
Karim Metwaly <88909362+KarimMetwaly19@users.noreply.github.com>
2024-04-19 19:37:03 +0200

    b1 fix

--- train_tryon.py
+++ train_tryon.py
@@ -1667,7 +1667,7 @@ class build_resunetplusplus(nn.Module):
         c3 = self.c3(c2)
         #c4 = self.c4(c3)
 
-        b1 = self.b1(c2)
+        b1 = self.b1(c3)
 
         #d1 = self.d1(c3, b1)
         d2 = self.d2(c2, b1)
