85c561baa76a8fc6851378cb22e2c7c2daa43baa
Mohamed Ahmed <88492114+SEmohamedAhmed@users.noreply.github.com>
2024-05-22 14:15:59 +0300

    Update train_tryon.py

--- train_tryon.py
+++ train_tryon.py
@@ -1645,7 +1645,6 @@ class build_resunetplusplus(nn.Module):
         self.output = nn.Conv2d(128, 4, kernel_size=1)
 		self.old_lr=opt.lr
 		self.old_lr_gmm=0.1*opt.lr
-    
 	# self.old_lr = opt.lr
 	# self.old_lr_gmm = 0.1*opt.lr
 
