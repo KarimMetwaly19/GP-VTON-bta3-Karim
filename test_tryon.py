1bbf4d14016c1f30385e5f030fc16d505d621667
Mohamed Ahmed <88492114+SEmohamedAhmed@users.noreply.github.com>
2024-07-01 00:39:01 +0300

    Update test_tryon.py

--- test_tryon.py
+++ test_tryon.py
@@ -20,6 +20,7 @@ def show_tryon(x, y, z, warped_prod_edge, w, k, i):
     print(w.shape)
     print(z.shape)
     print(warped_prod_edge.shape)
+    print(y.shape)
     combine = torch.cat([k[0], i[0]], 2).squeeze()
     cv_img = (combine.permute(1, 2, 0).detach().cpu().numpy() + 1) / 2
     rgb = (cv_img * 255).astype(np.uint8)
